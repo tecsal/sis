9e30983ae3f09e2ce9815b7f206c83ab52a6da06
clancien <clancien@tecsal.cl>
2019-12-16 17:10:36 -0300

    ayuda pacs: osirix

--- rebuild-pages.sh
+++ rebuild-pages.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-email="clancien@tecsal.cl"
-username="clancien"
-
 cd ~/workspace/sis/sis-cdn/
 
-git config user.email $email
-git config user.name $username
+#email="clancien@tecsal.cl"
+#username="clancien"
+#git config user.email $email
+#git config user.name $username
 
-git push origin master
+
+
 git commit -m 'rebuild pages' --allow-empty
+git push origin master
 
